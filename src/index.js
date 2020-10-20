6ac7e23f253b3f294d9dad811de86b2ccac20146
Vaibhav Negi <10vaibhavsinghnegi@gmail.com>
2020-10-20 14:02:57 +0530

    Serving public dir

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 const express = require('express')
+const path  = require('path')
+
 
 const app = express()
-
 const port = process.env.PORT || 3000
 
-app.get('',(req,res) => {
+// serving public folder
+const pubdir = path.join(__dirname,'../public')
+app.use(express.static(pubdir))
+
+// routes
+app.get('/hi',(req,res) => {
     res.send('Hello Express!!')
 })
 
+// server start
 app.listen(port,() => {
-    console.log('Server is up on port ',port);
+    console.log('Server is up on port ',port)
 })
